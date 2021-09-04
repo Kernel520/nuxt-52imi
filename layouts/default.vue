c1723709ec6507ab6391d30ddb0cbe9503aec257
kernel <52025811@Kernel>
2021-09-04 17:05:19 +0800

    TAT

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+        <NavBar />
+        <nuxt />
+    </div>
+</template>
+
+<script>
+import NavBar from '~/components/NavBar.vue';
+
+export default {
+    components: {
+        NavBar,
+    }
+}
+</script>
+
+<style>
+html {
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
--- pages/create.vue
+++ pages/create.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>
-      Kernel
+     Create an Kernel
     </h1>
   </div>
 </template>
